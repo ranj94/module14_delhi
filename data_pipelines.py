2c9aef870cf589d1d28865966e69a72a20e6abd0
auto-user <auto@example.com>
2026-08-14 10:14:34 +0100

    Publish processed data

--- data_pipelines.py
+++ data_pipelines.py
@@ -1,6 +1,7 @@
 from extract import extract_data
 from transform import transform_data
 from load import load_data
+from model import create_model
 
 def main():
     df_delhi = extract_data()
@@ -8,7 +9,14 @@ def main():
     load_data(repo_url = "https://github.com/ranj94//module14_delhi.git")  # publishes processed dataset
     print("Pipeline completed successfully!")
 
+def process_data():
+    df_delhi = extract_data()
+    delhi_processed = transform_data(df_delhi)
+    return delhi_processed
+
 if __name__ == "__main__":
     main()
 
 print("delhi done")
+
+##model_used = create_model(delhi_processed)
