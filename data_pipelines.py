be04a478ac967b932c5eeae45e34456a2434ab2a
auto-user <auto@example.com>
2026-08-14 11:56:49 +0100

    Publish processed data

--- data_pipelines.py
+++ data_pipelines.py
@@ -26,9 +26,9 @@ print(data_for_model)
 
 #df_delhi = extract_data()
 delhi_processed = transform_data(extract_data())
-print(delhi_processed)
+print("delhi_processed")
 model_used = create_model(delhi_processed)
 print("model_created + tested")
 
 forcasted = forecast(model_used, "2022-01-01", "2022-12-31")
-print(forcasted)
+##print(forcasted)
--- use_model.py
+++ use_model.py
@@ -1,5 +1,6 @@
 
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 import pandas as pd
 
 def forecast(model_name, start_date, end_date):
@@ -10,5 +11,8 @@ def forecast(model_name, start_date, end_date):
     future_forecast_aqi = pd.DataFrame(model_name.forecast(steps=(len(date_range) + 52))).reset_index()
     future_forecast_aqi = future_forecast_aqi.rename(columns={"index":"date", "predicted_mean":"predicted_aqi"})
     future_forecast_aqi = future_forecast_aqi.iloc[-(len(date_range)):]
+    plt.figure(figsize=(15, 5))
+    sns.lineplot(x="date", y= "predicted_aqi", data = future_forecast_aqi)
+    plt.show()
     return future_forecast_aqi
 ##future_forecast_aqi[(future_forecast_aqi["date"] >= "2022-01-01") & (future_forecast_aqi["date"] < "2023-01-01")]
