999e6596d5cd427a9aa929571647b3f25a6a548c
auto-user <auto@example.com>
2026-08-14 12:10:41 +0100

    Publish processed data

--- data_pipelines.py
+++ data_pipelines.py
@@ -11,23 +11,17 @@ def main():
     load_data(repo_url = "https://github.com/ranj94//module14_delhi.git")  # publishes processed dataset
     print("Pipeline completed successfully!")
 
-def process_data():
-    df_delhi = extract_data()
-    delhi_processed = transform_data(df_delhi)
-    ##return delhi_processed
-
 if __name__ == "__main__":
     main()
 
 print("delhi done")
 
-data_for_model = process_data
-print("process data")
+
 ##print(data_for_model)
 
 #df_delhi = extract_data()
-delhi_processed = data_for_model
-##delhi_processed = transform_data(extract_data())
+#delhi_processed = data_for_model
+delhi_processed = transform_data(extract_data())
 print("delhi_processed")
 model_used = create_model(delhi_processed)
 print("model_created + tested")
@@ -38,4 +32,4 @@ forcasted = forecast(model_used, "2022-01-01", "2022-12-31")
 
 ##print(forcasted)
 
-display(pd.DataFrame(data_for_model).head())
+display(pd.DataFrame(forcasted).head())
